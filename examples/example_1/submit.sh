33c794f871a734403001bad973142fad8ddb7cc7
ThibeauWouters <thibeauwouters@gmail.com>
2025-12-02 16:07:20 +0100

    WIP: breaking memory, need to update the passing of the kwargs

--- examples/example_1/submit.sh
+++ examples/example_1/submit.sh
@@ -2,8 +2,8 @@
 #Set job requirements
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -p gpu_a100
-#SBATCH -t 00:15:00
+#SBATCH -p gpu_h100
+#SBATCH -t 01:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-gpu=1
 #SBATCH --mem-per-gpu=10G
